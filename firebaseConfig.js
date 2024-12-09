1ed82171cca0a32dafbb558a91d2e5d5a055c4c5
Mukachxki <aaronfernandez383@gmail.com>
2024-12-09 20:14:51 +0800

    firebaseconfig

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,22 +1,23 @@
+//
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore'; 
 import 'firebase/compat/functions';
 
-
 const firebaseConfig = {
-  apiKey: 'AIzaSyCsv3CsVJL4ri6Gz6I3UiCfxatoYVWoxbw',
-  authDomain: 'thesis-ad543.firebaseapp.com',
-  projectId: 'thesis-ad543',
-  storageBucket: 'thesis-ad543.appspot.com', 
-  messagingSenderId: '1836074086378738059',
-  appId: '1:885904173811:android:ef336df3417631e854a586',
+  apiKey: "AIzaSyBPrqDFvjpeA27TX5torMOdo8P3hHFtZek",
+  authDomain: "thesis-ad543.firebaseapp.com",
+  projectId: "thesis-ad543",
+  storageBucket: "thesis-ad543.firebasestorage.app",
+  messagingSenderId: "885904173811",
+  appId: "1:885904173811:web:dd9d0c9787558da754a586",
+  measurementId: "G-4KMCGFD3XN"
 };
 
 if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 }
 
 export const auth = firebase.auth();
-export const db = firebase.firestore(); 
+export const db = firebase.firestore();
 export default firebase;
